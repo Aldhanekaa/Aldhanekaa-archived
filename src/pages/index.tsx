39253dbe9fba84db74ce58e448dad7c5ce239634
Aldhan (He/Him) <aldhanekadev@gmail.com>
2021-05-18 00:13:08 +0700

    initial

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Header from "../components/navbar"
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <section>
+        <div>
+          <h2>Aldhaneka</h2>
+          <h3>Web Developer and Backend Developer</h3>
+          <p>hello there</p>
+        </div>
+      </section>
+    </>
+  )
+}
